fa895a830c80af7c34c6bde46a0c4a874b92a5cb
core <jinjs6@gmail.com>
2023-03-08 18:47:55 +0900

    test: jdk 동적 프록시, CGLIB 프록시 캐스팅 성공 여부 테스트

---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,42 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); //JDK 동적 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //JDK 동적 프록시를 구현 클래스로 캐스팅 시도하면 싫패, ClassCastException 발생
+        assertThatThrownBy(() -> {
+                    MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+                })
+                .isInstanceOf(ClassCastException.class);
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); //CGLIB 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //CGLIB 프록시를 구현 클래스로 캐스팅 시도하면 성공
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
